e2509a2d5ecca91fe6dd99c3dc179528863e3aa8
ManoliuSilvia <manoliusilvia2002@gmail.com>
2023-01-13 00:10:50 +0200

    aval prod modif

--- ProductShipping/src/main/java/com/example/demo/model/DTO/ProductDTO.java
+++ ProductShipping/src/main/java/com/example/demo/model/DTO/ProductDTO.java
@@ -5,6 +5,29 @@ import com.example.demo.model.entity.Product;
 import java.util.List;
 
 public class ProductDTO {
-    public Product product;
+    public Integer id;
     public Integer quantity;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Integer productId, Integer quantity) {
+        this.id = productId;
+        this.quantity = quantity;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
--- ProductShipping/src/main/java/com/example/demo/model/controllers/ProductController.java
+++ ProductShipping/src/main/java/com/example/demo/model/controllers/ProductController.java
@@ -1,13 +1,16 @@
 package com.example.demo.model.controllers;
 
+import com.example.demo.model.DTO.ProductDTO;
 import com.example.demo.model.entity.Product;
 import com.example.demo.model.repositories.UserRepository;
 import com.example.demo.model.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -16,7 +19,7 @@ public class  ProductController {
     public ProductService productService;
     @Autowired
     private UserRepository userRepository;
-
+    private List<ProductDTO> selectedItems = new ArrayList<>();
     @GetMapping("/register_product")
     public String registerProduct(){
         return "register_product";
@@ -24,12 +27,21 @@ public class  ProductController {
     @GetMapping("/available_products")
     public ModelAndView getAllProducts(){
         List<Product> list = productService.getAllProducts();
-        return new ModelAndView("available_products", "products", list);
+        ModelAndView modelAndView =  new ModelAndView("available_products", "products", list);
+        modelAndView.getModelMap().addAttribute("selectedProduct",new ProductDTO());
+        return modelAndView;
     }
     @PostMapping("/save")
     public String addProduct(@ModelAttribute Product product){
         System.out.println("added product: " + product);
         productService.save(product);
         return "redirect:/available_products";
+    }
+    @PostMapping("/cart")
+    public String addOne(@ModelAttribute ProductDTO selectedProduct){
+        //Product product = productService.getProductsById(id);
+        ProductDTO productDTO = new ProductDTO();
+        selectedItems.add(selectedProduct);
+        return "available_products";
     }
 }
